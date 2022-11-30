ce3369c19999a883cddb8a85bcf7e42b1e7cce2e
Me <getdaniel>
2022-11-30 17:31:15 +0300

    optimize bottom navigation bar

--- geez_amharic/lib/main.dart
+++ geez_amharic/lib/main.dart
@@ -7,21 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Language Translation',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.teal,
       ),
       debugShowCheckedModeBanner: false,
@@ -33,15 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,23 +33,22 @@ class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
   static const List<Widget> _widgetOptions = <Widget>[
-    TextField(
-      obscureText: false,
-      maxLines: 15,
-      decoration: InputDecoration(
-        // labelText: "Ge'ez",
-        border: OutlineInputBorder(),
-        hintText: "Enter Ge'ez word(s) or sentence(s) for translation...",
+    Padding(
+      padding: EdgeInsets.all(5.0),
+      child: TextField(
+        obscureText: false,
+        maxLines: 15,
+        decoration: InputDecoration(
+          // labelText: "Ge'ez",
+          border: OutlineInputBorder(),
+          hintText: "Enter Ge'ez word(s) or sentence(s) for translation...",
+        ),
       ),
     ),
-    SizedBox(
+    Padding(
+      padding: EdgeInsets.all(5.0),
       child: Center(
-        child: Text("Google's Translation", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-      ),
-    ),
-    SizedBox(
-      child: Center(
-        child: Text("Learn About Translation", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+        child: Text("Learn About Ge'ez", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
       ),
     )
   ];
@@ -81,27 +61,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Column(
           children: [
-            Center(
-              // Center is a layout widget. It takes a single child and positions it
-              // in the middle of the parent.
-              child: _widgetOptions.elementAt(_selectedIndex),
-            ),
+            _widgetOptions.elementAt(_selectedIndex),
           ],
         ),
       ),
@@ -157,12 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
             backgroundColor: Colors.teal,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.language),
-            backgroundColor: Colors.teal,
-            label: 'Google Translation',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notes_sharp),
+            icon: Icon(Icons.school),
             backgroundColor: Colors.teal,
             label: "Learn Ge'ez",
           ),
