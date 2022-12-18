b5113a26bfb7ed1facc34e1ba51a5e57c453abbe
Me <getdaniel>
2022-12-18 23:30:43 +0300

    detail about our group

--- lib/about_us.dart
+++ lib/about_us.dart
@@ -1,3 +1,4 @@
+import 'package:contactus/contactus.dart';
 import 'package:flutter/material.dart';
 
 class About extends StatefulWidget {
@@ -12,16 +13,32 @@ class _HomeState extends State<About> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Center(
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            "Developed By: Daniel G.",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ContactUs(
+          logo: const AssetImage('assets/images/ic_launcher.png'),
+          avatarRadius: 100,
+          companyFontSize: 32,
+          companyName: 'Simple Language Translator',
+          companyColor: Colors.teal,
+          companyFontWeight: FontWeight.bold,
+          tagLine: "Daniel G, Yezibalem A. Yeabsira A.",
+          taglineColor: Colors.black,
+          taglineFontWeight: FontWeight.bold,
+          cardColor: Colors.teal,
+          emailText: "Gmail",
+          email: 'danielgetaneh2011@gmail.com',
+          githubUserName: 'getdaniel',
+          dividerThickness: 10,
+          textColor: Colors.white,
+          dividerColor: Colors.teal,
+          linkedinURL: 'https://www.linkedin.com/in/daniel-getaneh-50b459175/',
+          phoneNumberText: 'Phone Number',
+          phoneNumber: '+251935976224',
+          instagram: 'https://www.instagram.com/getdaniel3/',
+          twitterHandle: 'https://twitter.com/DanielGetanehS',
+          website: 'https://getdaniel.github.io/my_profile/',
+          websiteText: 'Website',
         ),
       ),
     );
